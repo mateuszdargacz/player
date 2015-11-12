92539cc4fd97ff6438a17d8cd070c448d5e85e87
mateuszdargacz <mateuszdargacz@gmail.com>
2015-11-12 18:43:00 +0100

    lol removal

--- frontend/static/angular_app/app.js
+++ frontend/static/angular_app/app.js
@@ -14,10 +14,10 @@ angular
         ctrls: "/static/angular_app/templates/ctrls",
         tmpl: "/static/MusikTemplate"
     })
-    //.constant('backendEndpoint', '37.187.58.217')
-    //.constant('backendPort', '9001')
-    .constant('backendEndpoint', '127.0.0.1')
-    .constant('backendPort', '9000')
+    .constant('backendEndpoint', '37.187.58.217')
+    .constant('backendPort', '9001')
+    //.constant('backendEndpoint', '127.0.0.1')
+    //.constant('backendPort', '9000')
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $animateProvider, path) {
         $urlRouterProvider.otherwise("/index");
         $urlRouterProvider.when('', '/index');
